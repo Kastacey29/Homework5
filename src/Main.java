822977dee059afb7b6f97cf9edc9939638676982
Stacey29 <Kastacey@yandex.ru>
2022-08-11 22:00:21 +0300

    сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        int [] arr1 = new int [3];
+        arr1 [0] = 1;
+        arr1 [1] = 2;
+        arr1 [2] = 3;
+        float [] arr2 = {1.57f, 7.654f, 9.986f};
+        char [] arr3 = {'f', 25, 'h', 36};
+        for (int i = 0; i < 3; i++) {
+            if (i == (arr1.length - 1)) {
+                System.out.println(arr1[i] + " ");
+            } else {
+                System.out.print(arr1[i] + ", ");
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            if (i == (arr2.length - 1)) {
+                System.out.println(arr2[i] + " ");
+            } else {
+                System.out.print(arr2[i] + ", ");
+            }
+        }
+        for (int i = 0; i < arr3.length; i++) {
+            if (i == (arr3.length - 1)) {
+                System.out.println(arr3[i] + " ");
+            } else {
+                System.out.print(arr3[i] + ", ");
+            }
+        }
+        for (int i = arr1.length-1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr1[i] + " ");
+            } else {
+                System.out.print(arr1[i] + ", ");
+            }
+        }
+        for (int i = arr2.length-1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr2[i] + " ");
+            } else {
+                System.out.print(arr2[i] + ", ");
+            }
+        }
+        for (int i = arr3.length-1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(arr3[i] + " ");
+            } else {
+                System.out.print(arr3[i] + ", ");
+            }
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i]%2!=0) {
+                if (i == (arr1.length - 1)) {
+                    System.out.println((arr1[i] + 1) + " ");
+                } else {
+                    System.out.print((arr1[i] + 1) + ", ");
+                }
+            } else if (i == (arr1.length - 1)) {
+                System.out.println(arr1[i] + " ");
+            } else {
+                System.out.print(arr1[i] + ", ");
+        }
+                }
+
     }
 }
